b6ebd515b20c0265da79f9c57d1eec2de7008f76
James Robertson <james@jamesrobertson.eu>
2019-11-16 15:19:56 +0000

    bug fix: Each page length is now correctly calculated.

--- lib/qnd_html2page.rb
+++ lib/qnd_html2page.rb
@@ -8,14 +8,31 @@ require 'tempfile'
 require 'rxfhelper'
 
 
+module ArraySlices
+
+  refine Array do
+
+      def slice_at(*indices)
+
+        a = indices
+        a << -1 if a[-1] != self[-1]
+        a.unshift -1
+        a.each_cons(2).map {|x1, x2| self.slice(x1+1..x2) }
+
+      end
+    
+  end
+end
 
 class QndHtml2Page
+  using ArraySlices
+  using ColouredText
 
   attr_reader :to_pages
 
-  def initialize(html, debug: false, pg_height: 676)
+  def initialize(html, debug: false, pg_height: 770, width: '700px')
 
-    @html, @height, @debug = html, pg_height, debug
+    @html, @height, @width, @debug = html, pg_height, width, debug
     @to_pages = scan(RXFHelper.read(@html).first)
 
   end
@@ -28,11 +45,12 @@ class QndHtml2Page
 
     doc = Rexle.new(html)
     body = doc.root.element('body')
+    body.attributes[:style] = 'width: ' + @width
   
     count = 0
     body.each_recursive do |e|
 
-      puts 'e: ' + e.name if @debug
+      puts ('e: ' + e.xml).debug if @debug
       ignore_list = %w(span b li tr td dt dd em strong i a)
       next if ignore_list.include? e.name
       span = Rexle::Element.new('span').add_text(count.to_s)
@@ -49,46 +67,51 @@ class QndHtml2Page
     File.write tmpfile.path + '.html', doc.root.xml
 
     browser = Ferrum::Browser.new
+    #browser.resize width: 300, height: 300
     browser.goto('file://' + tmpfile.path + '.html')
+    browser.screenshot(path: "/tmp/page.jpg") 
     span_list = browser.xpath('//span')
     a = span_list.map {|x| [x.text, x.find_position.last] }
 
-    offset = 0
+    
+    heights = ((a.last.last) / @height).round.to_i.times.inject([@height]) {|r, x| r << (r.last + @height)  } 
+    puts ('heights: ' + heights.inspect).debug if @debug
+    height = heights.shift
 
     a2 = a.inject([[]]) do |r,x|
 
-      puts 'r: ' + x.inspect if @debug
-      puts 'x: ' + x.inspect if @debug
-      x.last < offset + @height ? (r.last << x) : (offset = x.last; r << [x])
+      puts ('r: ' + x.inspect).debug if @debug
+      puts ('x: ' + x.inspect).debug if @debug
+      puts ('height: ' + height.inspect).debug if @debug
+      
+      x.last < height ? (r.last << x) : (height = heights.shift; r << [x])
       r
 
     end
 
 
     elements = doc.root.element('body').elements.to_a
-    puts 'elements.length: ' + elements.length.inspect if @debug
-    offset = 0
+    puts ('elements.length: ' + elements.length.inspect).debug if @debug
+    offset2 = 0
 
-    puts 'a2: ' + a2.inspect if @debug
-
-    pages = a2.map do |x|
-
-      id = x.last.first
-
-      puts 'id: ' + id.inspect if @debug
-      puts 'offset: ' + offset.inspect if @debug
-
-      a3 = elements[offset..-1].take_while do |e| 
-        puts 'e.text: ' + e.text.inspect
-        e.text != id
-      end
-
-      offset = a3.length 
-      div = Rexle::Element.new 'div'
-      a3.reject! {|e| e.name == 'span' and e.attributes[:class] == 'qndhtml2pg' }
-      a3.each {|e| div.add e}
-      div
+    puts ('a2: ' + a2.inspect).debug if @debug
+    
+    # find each last record span stop using the given id
+    stops = a2.map do |x|
+      elements.index(elements.find {|e| e.text == x.last.first })
     end
+    
+    puts ('stops: ' + stops.inspect).debug if @debug
+    
+    pages = elements.slice_at(*stops).map do |e_list|
+
+      div = Rexle::Element.new 'div'
+      e_list.reject! {|e| e.name == 'span' and e.attributes[:class] == 'qndhtml2pg' }
+      next if e_list.empty?
+      e_list.each {|e| div.add e}
+      
+      div
+    end.compact
 
     @to_pages = pages
 
